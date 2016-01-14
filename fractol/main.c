f90e13bf4f9c86ddc273dd4fca92122090396112
svelhinh <svelhinh@student.42.fr>
2016-01-14 12:40:53 +0100

    fractol cacoo commence

--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: svelhinh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 14:05:10 by svelhinh          #+#    #+#             */
-/*   Updated: 2016/01/13 17:35:00 by svelhinh         ###   ########.fr       */
+/*   Updated: 2016/01/14 12:40:31 by svelhinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,41 +21,48 @@ int		main(void)
 	void			*win;
 
 	// On definit la zone que l'on dessine. Ici, la fractale entiere
-	float			x1 = -1.5;
-	float			x2 = 1.5;
-	float			y1 = -1.5;
-	float			y2 = 1.5;
-	float			zoom = 500;		// Pour une distance de 1 sur le plan, on a 100 pixels sur l'image
+	float			x1 = -2.2;//-2.1;
+	float			x2 = 0.8;//0.6;
+	float			y1 = -1.2;//-1.2;
+	float			y2 = 1.2;//1.2;
+	float			img_x = 2000;
+	float			img_y = 1300;
 
-	// On calcule la taille de l'image
-	float			ximg = (x2 - x1) * zoom;
-	float			yimg = (y2 - y1) * zoom;
+	// On calcule le zoom de l'image
+	float			zoom_x = img_x / (x2 - x1);
+	float			zoom_y = img_y / (y2 - y1);
 
-	int				x = 0;
 	int				y;
+	int				x;
 	float complex	c;
 	float complex	z;
+	int				i;
+	int				i_max = 50;
 	int				color;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 2000, 1300, "fractol");
-	while (x < ximg)
+	y = 0;
+	while (y < img_y)
 	{
-		y = 0;
-		while (y < yimg)
+		x = 0;
+		while (x < img_x)
 		{
-			c = /*(x / zoom + x1) + (y / zoom + y1) * _Complex_I*/-0.0986 + 0.65186 * _Complex_I;
-			z = (x / zoom + x1) + (y / zoom + y1) * _Complex_I;
+			z = (x / zoom_x + x1) + (y / zoom_y + y1) * _Complex_I;
+			c = z;//-0.772691322542185 + 0.124281466072787 * _Complex_I;
 			color = 0x000000;
-			while (creal(z) * creal(z) + cimag(z) * cimag(z) < 4)
+			i = 0;
+			while (creal(z) * creal(z) + cimag(z) * cimag(z) < 4 && i < i_max)
 			{
-				z = z*z + c;
-				color += 10;
+				z = cpow(z, 2) + c;
+				color += 0x404040;
+				i++;
 			}
-			mlx_pixel_put(mlx, win, x, y, color);
-			y++;
+			if (i == i_max)
+				mlx_pixel_put(mlx, win, x, y, color);
+			x++;
 		}
-		x++;
+		y++;
 	}
 	mlx_loop(mlx);
 	return (0);
