307f26e8e83a5925743cd74bfa78056dab47caf6
svelhinh <svelhinh@student.42.fr>
2016-03-02 20:09:29 +0100

    rtv1 toujour une ellipse

--- rtv1/srcs/render.c
+++ rtv1/srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: svelhinh <svelhinh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 11:21:40 by svelhinh          #+#    #+#             */
-/*   Updated: 2016/03/02 19:13:53 by svelhinh         ###   ########.fr       */
+/*   Updated: 2016/03/02 20:09:09 by svelhinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	sphere(t_rt *rt)
 	float	b;
 	float	c;
 	float	d;
-	float	t;
 
 	a = pow(rt->vdir.x, 2) + pow(rt->vdir.y, 2) + pow(rt->vdir.z, 2);
 	b = 2 * (rt->vdir.x * (rt->campos.x - rt->sphere.x) + rt->vdir.y *
@@ -31,14 +30,7 @@ void	sphere(t_rt *rt)
 	if (d < 0)
 		mlx_pixel_put(rt->mlx, rt->win, rt->vdir.x, rt->vdir.y, 0);
 	else
-	{
-		if (d > 0)
-			t = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
-		else if (d == 0)
-			t = -b / (2 * a);
-		mlx_pixel_put(rt->mlx, rt->win, rt->campos.x + t * rt->vdir.x,
-				rt->campos.y + t * rt->vdir.y, 0xffffff);
-	}
+		mlx_pixel_put(rt->mlx, rt->win, rt->vdir.x, rt->vdir.y, 0xffffff);
 }
 
 void	render(t_rt *rt)
@@ -51,11 +43,11 @@ void	render(t_rt *rt)
 	rt->campos.x = 0;
 	rt->campos.y = 0;
 	rt->campos.z = -10;
-	rt->sphere.x = 20;
-	rt->sphere.y = 20;
-	rt->sphere.z = 0;
-	rt->sphereradius = 5;
-	while (z < 25)
+	rt->sphere.x = SW / 2;
+	rt->sphere.y = SH / 2;
+	rt->sphere.z = 75;
+	rt->sphereradius = 50;
+	while (z < 50)
 	{
 		y = 0;
 		while (y < SH)
@@ -63,9 +55,9 @@ void	render(t_rt *rt)
 			x = 0;
 			while (x < SW)
 			{
-				rt->vdir.x = x;
-				rt->vdir.y = y;
-				rt->vdir.z = z;
+				rt->vdir.x = x - rt->campos.x;
+				rt->vdir.y = y - rt->campos.y;
+				rt->vdir.z = z - rt->campos.z;
 				sphere(rt);
 				x++;
 			}
