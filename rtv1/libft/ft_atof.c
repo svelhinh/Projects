fa52d847f042153563ea9dcdc2b842f73e878f10
svelhinh <svelhinh@student.42.fr>
2016-03-09 15:35:24 +0100

    rtv1 atof reste a gerer nbr negatifs

--- rtv1/libft/ft_atof.c
+++ rtv1/libft/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: svelhinh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/09 14:13:24 by svelhinh          #+#    #+#             */
-/*   Updated: 2016/03/09 14:54:09 by svelhinh         ###   ########.fr       */
+/*   Updated: 2016/03/09 15:34:47 by svelhinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,18 @@ double			ft_atof(char *s)
 	i = 0;
 	while (s[i] != '.' && s[i])
 	{
-		if (!ft_isdigit(s[i]))
+		if (!ft_isdigit(s[i]) && s[i] != '-')
 			return (0);
 		i++;
 	}
+	if (!s[i])
+		return (ft_atoi(s));
 	it = ft_strsub(s, 0, i);
+	ft_putendl(it);
 	nb = ft_atoi(it);
 	dot = i;
 	i++;
-	while (s[i] && s[i])
+	while (s[i])
 	{
 		if (!ft_isdigit(s[i]))
 			return (0);
