5718314b75906b698302a8621ecffa507925a498
svelhinh <svelhinh@student.42.fr>
2016-03-21 20:27:18 +0100

    rtv1 ouais en fait peut etre pas

--- rtv1/srcs/light.c
+++ rtv1/srcs/light.c
@@ -6,7 +6,7 @@
 /*   By: svelhinh <svelhinh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 11:26:59 by svelhinh          #+#    #+#             */
-/*   Updated: 2016/03/21 18:55:05 by svelhinh         ###   ########.fr       */
+/*   Updated: 2016/03/21 20:26:50 by svelhinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,24 +47,25 @@ void	light_sphere(t_rt *rt, float t, float tmp, int currentobj)
 
 void	light_plane(t_rt *rt, float t, float tmp, int currentobj)
 {
-	t_vector3d		light_vec;		// Vecteur de la lumiere
+	/*t_vector3d		light_vec;		// Vecteur de la lumiere
 	t_vector3d		inter;			// Point d'intersection
 	float			angle;			// angle entre les 2 rayons (camera et lumiere)
 	float			inter_dist;		// variable utilise pour normaliser le vecteur au point d'intersection
 	float			light_dist;		// distance de la lumiere au point d'intersection
 
 	(void)tmp;
-	printf("%f\n", t);
+	//printf("%f\n", t);
 	//printf("start.x : %f\nstart.y : %f\nstart.z : %f\n\n", rt->r.dir.x, rt->r.dir.y, rt->r.dir.z);
 	inter.x = rt->r.start.x + rt->r.dir.x * t;		// Calcul du point d'intersection
 	inter.y = rt->r.start.y + rt->r.dir.y * t;
 	inter.z = rt->r.start.z + rt->r.dir.z * t;
-	printf("start.x : %f\nstart.y : %f\nstart.z : %f\n\n", inter.x, inter.y, inter.z);
 	light_vec = vectorsub(&rt->light.pos, &inter, 0);	// Calcul du vecteur de la lumiere
+	//printf("light_dist : %f\n\n", light_dist);
 	light_dist = sqrt(pow(light_vec.x, 2) + pow(light_vec.y, 2) + pow(light_vec.z, 2));		// Normalisation du vecteur de la lumiere
 	light_vec.x /= light_dist;
 	light_vec.y /= light_dist;
 	light_vec.z /= light_dist;
+	//printf("light_vec.x : %f\nlight_vec.y : %f\nlight_vec.z : %f\n\n", light_vec.x, light_vec.y, light_vec.z);
 	inter_dist = sqrt(pow(inter.x, 2) + pow(inter.y, 2) + pow(inter.z, 2));		// Normalisation du vecteur de l'intersection
 	inter.x /= inter_dist;
 	inter.y /= inter_dist;
@@ -77,8 +78,8 @@ void	light_plane(t_rt *rt, float t, float tmp, int currentobj)
 		rt->global_color.red += angle * rt->light.intensity.red * rt->p[currentobj].color.red;		// Calcul des couleurs
 		rt->global_color.green += angle * rt->light.intensity.green * rt->p[currentobj].color.green;
 		rt->global_color.blue += angle * rt->light.intensity.blue * rt->p[currentobj].color.blue;
-	}
-	/*t_vector3d	scaled;
+	}*/
+	t_vector3d	scaled;
 	t_vector3d	inter;
 	t_vector3d	normal;
 	t_vector3d	light_vec;
@@ -94,6 +95,7 @@ void	light_plane(t_rt *rt, float t, float tmp, int currentobj)
 		tmp = 1 / sqrt(tmp);
 		normal = vectorscale(tmp, &normal);
 		light_vec = vectorsub(&rt->light.pos, &inter, 0);
+		printf("yo : %f\n", vectordot(&normal, &light_vec, 0));
 		if (vectordot(&normal, &light_vec, 0) > 0)
 		{
 			tmp = sqrt(vectordot(&light_vec, &light_vec, 0));
@@ -101,16 +103,12 @@ void	light_plane(t_rt *rt, float t, float tmp, int currentobj)
 			{
 				light_ray.dir = vectorscale((1 / tmp), &light_vec);
 				angle = vectordot(&light_ray.dir, &normal, 0);
-				if (angle > 0)
-				{
-					printf("angle : %f\n", angle);
-					rt->global_color.red += angle * rt->light.intensity.red * rt->p[currentobj].color.red;
-					rt->global_color.green += angle * rt->light.intensity.green * rt->p[currentobj].color.green;
-					rt->global_color.blue += angle * rt->light.intensity.blue * rt->p[currentobj].color.blue;
-				}
+				rt->global_color.red += angle * rt->light.intensity.red * rt->p[currentobj].color.red;
+				rt->global_color.green += angle * rt->light.intensity.green * rt->p[currentobj].color.green;
+				rt->global_color.blue += angle * rt->light.intensity.blue * rt->p[currentobj].color.blue;
 			}
 		}
-	}*/
+	}
 }
 
 void	light_cylinder(t_rt *rt, float t, float tmp, int currentobj)
