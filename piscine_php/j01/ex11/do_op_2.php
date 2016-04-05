9816288615bf9cea5892f261d8e693ec375570b3
svelhinh <svelhinh@student.42.fr>
2016-04-05 17:58:19 +0200

    php j01 ex11

--- piscine_php/j01/ex11/do_op_2.php
+++ piscine_php/j01/ex11/do_op_2.php
@@ -17,11 +17,76 @@ function ft_split($str)
 	return $str2;
 }
 
+function check_errors($str)
+{
+	$i = 0;
+	while ($i < strlen($str))
+	{
+		if (!is_numeric($str[$i]) && $str[$i] != "+" && $str[$i] != "-" && $str[$i] != "*" && $str[$i] != "/" && $str[$i] != "%" && $str[$i] != " ")
+			return 1;
+		$i++;
+	}
+	return 0;
+}
+
+function many_op($str)
+{
+	if (count($str) != 2)
+		return 1;
+	return 0;
+}
+
 if ($argc == 2)
 {
-	$tab = ft_split($argv[1]);
-	print_r($tab);
-//	echo "$nb\n";
+	$str = $argv[1];
+	if (!check_errors($str))
+	{
+		if (strchr($str, "+"))
+		{
+			$op = 0;
+			$str = explode("+", $str);
+		}
+		else if (strchr($str, "-"))
+		{
+			$op = 1;
+			$str = explode("-", $str);
+		}
+		else if (strchr($str, "*"))
+		{
+			$op = 2;
+			$str = explode("*", $str);
+		}
+		else if (strchr($str, "/"))
+		{
+			$op = 3;
+			$str = explode("/", $str);
+		}
+		else if (strchr($str, "%"))
+		{
+			$op = 4;
+			$str = explode("%", $str);
+		}
+		$nb = ft_split($str[0]);
+		$nb2 = ft_split($str[1]);
+		if ($nb && $nb2)
+		{
+			if ($op == 0)
+				$nb3 = $nb[0] + $nb2[0];
+			if ($op == 1)
+				$nb3 = $nb[0] - $nb2[0];
+			if ($op == 2)
+				$nb3 = $nb[0] * $nb2[0];
+			if ($op == 3)
+				$nb3 = $nb[0] / $nb2[0];
+			if ($op == 4)
+				$nb3 = $nb[0] % $nb2[0];
+			echo "$nb3\n";
+		}
+		else
+			echo "Syntax Error\n";
+	}
+	else
+		echo "Syntax Error\n";
 }
 else
 	echo "Incorrect Parameters\n";
