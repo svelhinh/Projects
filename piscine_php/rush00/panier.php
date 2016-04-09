6b0d1cd0ca2e8f4edb459fb48c8940a511119b1a
svelhinh <svelhinh@student.42.fr>
2016-04-09 20:53:36 +0200

    php rush00 panier fini

--- piscine_php/rush00/panier.php
+++ piscine_php/rush00/panier.php
@@ -6,15 +6,15 @@ $_POST["image"] = "images/vins/blanc/blanc-bordeaux-nadeau.jpg";
 $_POST["nom"] = "Nadeau";
 $_POST["quantite"] = "2";
 $_POST["prix"] = "3.5";
-$_POST['submit'] = "Ajouter";
 
-if ($_POST['submit'] == "Ajouter")
+if ($_POST && $_POST["submit"] == "Ajouter")
 {
 	if (!$_SESSION['panier'])
 		$_SESSION['panier'] = array($_POST);
 	else
 		array_push($_SESSION['panier'], $_POST);
 }
+
 else if ($_POST['submit'] == "Vider")
 	unset($_SESSION['panier']);
 ?>
@@ -26,11 +26,17 @@ else if ($_POST['submit'] == "Vider")
 		<link rel="stylesheet" href="css/panier.css" />
 	</head>
 	<body>
+		<form method="post">
+			<input type="submit" name="submit" value="Ajouter" />
+		</form>
 		<div class="panier">
 			<img style="width: 30%;" src="images/panier.png" />
-			<div class="produits">
 <?php
 if ($_SESSION['panier'])
+{
+	echo '<div class="produits">';
+	$total = 0;
+	$quantite = 0;
 	foreach ($_SESSION['panier'] as $products)
 	{
 		echo '<div>';
@@ -39,19 +45,23 @@ if ($_SESSION['panier'])
 		echo '<div class="quantite">'.$products["quantite"].'</div>';
 		echo '<div class="prix">'.$products["prix"].'&euro;</div>';
 		echo '</div>';
-		echo '<hr />';
+		echo '<hr class="line" />';
+		$total += $products["prix"];
+		$quantite += $products["quantite"];
 	}
-?>
-				<input class="valider" type="button" value="Valider" />
-<?php
-if (!$_SESSION['panier'])
-{
+	echo '<div class="total">TOTAL : </div>';
+	echo '<div class="quantite">'.$quantite.'</div>';
+	echo '<div class="prix">'.$total.'&euro;</div>';
+	echo '<hr class="line" />';
+	echo '<input class="valider" type="button" value="Valider" />';
 	echo '<form method="post">';
-	echo '<input class="vider" type="image" name="submit" src="images/corbeille.jpg" value="Vider" />';
+	echo '<input class="corbeille" type="image" name="submit" src="images/corbeille.jpg" value="Vider" />';
 	echo '</form>';
+	echo '</div>';
 }
+else
+	echo '<div class="vide"><p class="panier_vide">PANIER VIDE</p></div>';
 ?>
-			</div>
 		</div>
 	</body>
 </html>
